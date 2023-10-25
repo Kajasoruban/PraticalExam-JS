d8168b852f5c76d7e35119bde18baa26be9cf713
Kajasoruban <kajan.bluwin@gmail.com>
2023-10-25 15:59:47 +0530

    day1

---
+++ practice1.js
@@ -0,0 +1,47 @@
+// //q1
+// var emp = {};
+
+// //q2
+
+// function introduceYou() {
+//     console.log('Hello,my name is '+person.name)
+     
+
+// }
+
+// var person = {name:"kajan",age:21,function:introduceYou};
+
+
+
+
+// console.log(introduceYou());
+
+// //q3
+
+// var student = {stname:'kajan'};
+
+// function greetStudent() {
+//     console.log('Hello '+student.stname+' Welcome to the Coding School')
+
+// }
+// console.log(greetStudent());
+
+// //q4
+
+// console.log(person.age);
+// // console.log(person);
+
+// //q5
+
+// person.email='john@gamil.com'
+// console.log(person);
+
+// //q6
+
+
+// //q7
+
+// console.log(match(person.name))
+
+// //q8
+//  var course
